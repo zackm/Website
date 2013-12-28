e209c5a7710f1e56b0eac06117d5ee23113030f6
Zack Mayeda <zack.mayeda@gmail.com>
2013-12-27 16:11:40 -0800

    remove old nav js

--- js/index.js
+++ js/index.js
@@ -1,30 +1,13 @@
 $(document).ready(function(){
 	
-// Nav Button Highlighting
-$("div.nav-button").click(function() {
-  $('div.nav-button').removeClass('nav-selected');
-  $(this).addClass('nav-selected');
-});
-// Home Nav Button
-$('#logo').click(function() {
-  $('div.nav-button').removeClass('nav-selected');
-});
-
-// Change page content
-var pages = ['home','about','work','contact'];
-
-$.each(pages,function(i,val){
-    var name = '.' + val;
-    $(name+'-button').click(function() {
-      var currentPage = $('.active-content');
-      if(!currentPage.hasClass(val)) {
-        currentPage.fadeToggle(400,"swing", function(){
-          currentPage.removeClass('active-content');
-          $(name).addClass('active-content');
-          $(name).fadeToggle(400);
-        });
-      }
-    });
-});
+  // Nav Button Highlighting
+  $("div.nav-button").click(function() {
+    $('div.nav-button').removeClass('nav-selected');
+    $(this).addClass('nav-selected');
+  });
+  // Home Nav Button
+  $('#logo').click(function() {
+    $('div.nav-button').removeClass('nav-selected');
+  });
 
 });
